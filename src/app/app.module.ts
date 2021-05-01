6d2eb6faec218f26569bb747ab47455ab2df1dfd
hanyang1986 <hanyang1986@gmail.com>
2021-05-01 15:31:57 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
